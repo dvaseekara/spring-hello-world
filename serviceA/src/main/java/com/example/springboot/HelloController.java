8e45457047ec2e8b906c8823932d08e2f1a9d133
Daniel Vaseekaran <dvaseeka@redhat.com>
2023-02-28 10:57:51 -0500

    updated ServceA to call Servcie B

--- serviceA/src/main/java/com/example/springboot/HelloController.java
+++ serviceA/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,26 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
+	@Value("${URL}")
+	String serviceburl;
 
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot Service A!";
+	}
+
+	@GetMapping("/hitB")
+	public String hitB(){
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response  = restTemplate.getForEntity(serviceburl + "/", String.class);
+		return response.getBody();
 	}
 
 }
