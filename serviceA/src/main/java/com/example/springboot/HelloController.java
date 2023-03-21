54871aed0320b4b2684aadc999f76f393d340c13
dvaseekara <53438081+dvaseekara@users.noreply.github.com>
2023-03-21 07:50:58 -0400

    Update HelloController.java

--- serviceA/src/main/java/com/example/springboot/HelloController.java
+++ serviceA/src/main/java/com/example/springboot/HelloController.java
@@ -21,6 +21,31 @@ public class HelloController {
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response  = restTemplate.getForEntity(serviceburl + "/", String.class);
 		return response.getBody();
+		
+		
+	}
+
+	@GetMapping("/health")
+	Map<String, Object> echo(HttpServletRequest request,
+		     @RequestHeader HttpHeaders headers,
+		     @RequestBody(required = false) Map<String, Object> body) {
+
+		Instant now = Instant.now();
+		Cookie[] cookies = request.getCookies();
+
+		Map<String, Object> result = new LinkedHashMap<>();
+		result.put("clientIpAddress", request.getRemoteAddr());
+		result.put("cookies", cookies == null ? emptyList()
+						      : Arrays.stream(cookies).toList());
+		result.put("headers", headers.toSingleValueMap()); // simplify json response
+		result.put("httpVersion", request.getProtocol());
+		result.put("method", request.getMethod());
+		result.put("body", body);
+		result.put("queryString", request.getQueryString());
+		result.put("startedDateTime", now);
+		result.put("url", request.getRequestURL());
+
+		return result;
 	}
 
 }
