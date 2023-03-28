bdba5ccc635445c6eb99d01d63b7a8b05ad01503
Daniel Vaseekaran <dvaseeka@redhat.com>
2023-03-28 10:50:00 -0400

    updated health endpoint

--- serviceA/src/main/java/com/example/springboot/HelloController.java
+++ serviceA/src/main/java/com/example/springboot/HelloController.java
@@ -1,10 +1,22 @@
 package com.example.springboot;
 
+import java.net.http.HttpHeaders;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloController {
@@ -17,27 +29,26 @@ public class HelloController {
 	}
 
 	@GetMapping("/hitB")
-	public String hitB(){
+	public String hitB() {
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> response  = restTemplate.getForEntity(serviceburl + "/", String.class);
+		ResponseEntity<String> response = restTemplate.getForEntity(serviceburl + "/", String.class);
 		return response.getBody();
-		
-		
+
 	}
 
 	@GetMapping("/health")
 	Map<String, Object> echo(HttpServletRequest request,
-		     @RequestHeader HttpHeaders headers,
-		     @RequestBody(required = false) Map<String, Object> body) {
+			@RequestHeader HttpHeaders headers,
+			@RequestBody(required = false) Map<String, Object> body) {
 
 		Instant now = Instant.now();
 		Cookie[] cookies = request.getCookies();
 
 		Map<String, Object> result = new LinkedHashMap<>();
 		result.put("clientIpAddress", request.getRemoteAddr());
-		result.put("cookies", cookies == null ? emptyList()
-						      : Arrays.stream(cookies).toList());
-		result.put("headers", headers.toSingleValueMap()); // simplify json response
+		result.put("cookies", cookies == null ? new ArrayList()
+				: Arrays.stream(cookies).toList());
+		result.put("headers", headers.allValues(serviceburl)); // simplify json response
 		result.put("httpVersion", request.getProtocol());
 		result.put("method", request.getMethod());
 		result.put("body", body);
